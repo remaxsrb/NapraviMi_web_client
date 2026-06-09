452ab8709faf519418ea26c7dd47494f63e8ef41
Marko Jovanovic <marko.jovanovic@quectel.com>
2026-06-09 13:07:39 +0200

    Product creation subcomponent added for crastsman

--- src/app/components/user/user-dashboard/user-dashboard.ts
+++ src/app/components/user/user-dashboard/user-dashboard.ts
@@ -2,19 +2,23 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 import { User } from '../../../models/user';
 import { AuthService } from '../../../services/utils/auth-service';
 import { UserActions } from '../../common/user-actions/user-actions';
+import { AddProduct } from '../../products/add-product/add-product';
 
 @Component({
   selector: 'app-user-dashboard',
   standalone: true,
-  imports: [CommonModule, MenubarModule, ButtonModule, UserActions],
+  imports: [CommonModule, MenubarModule, ButtonModule, UserActions, AddProduct],
   templateUrl: './user-dashboard.html',
   styleUrl: './user-dashboard.css',
 })
 export class UserDashboard implements OnInit {
   user: User = new User();
+  menuItems: MenuItem[] = [];
+  showAddProduct = false;
 
   constructor(private authService: AuthService) {}
 
@@ -22,6 +26,16 @@ export class UserDashboard implements OnInit {
     const storedUser = localStorage.getItem('userData');
     if (storedUser) {
       this.user = JSON.parse(storedUser) as User;
+    }
+
+    if (this.authService.get_role() === 'craftsman') {
+      this.menuItems = [
+        {
+          label: 'Dodaj proizvod',
+          icon: 'pi pi-plus',
+          command: () => { this.showAddProduct = !this.showAddProduct; },
+        },
+      ];
     }
   }
 
---
+++ src/app/services/product/product-service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private apiUrl = 'http://localhost:8080/products';
+
+  constructor(private http: HttpClient) {}
+
+  create(data: any): Observable<any> {
+    const token = localStorage.getItem('authToken');
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.post<any>(`${this.apiUrl}/create`, data, { headers });
+    return this.http.post<any>(`${this.apiUrl}/create`, data);
+  }
+
+  all(craftsmanId: number, skip: number, limit: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/all/${craftsmanId}?skip=${skip}&limit=${limit}`);
+  }
+}
