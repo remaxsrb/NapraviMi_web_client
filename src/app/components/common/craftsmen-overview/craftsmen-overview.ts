b14ca3accd79cb42c75f52fc88c1506bde0390d9
Marko <remaxsrb@protonmail.com>
2026-06-10 09:41:09 +0200

    Removed duplicate class

--- src/app/components/common/craftsmen-overview/craftsmen-overview.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-craftsmen-overview',
-  imports: [],
-  templateUrl: './craftsmen-overview.html',
-  styleUrl: './craftsmen-overview.css',
-})
-export class CraftsmenOverview {}
