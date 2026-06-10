54990de56b2ffe6451a8aa7a847cfe0f26919c98
Marko <remaxsrb@protonmail.com>
2026-06-10 09:40:36 +0200

    refactored user model to use inheritance

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,14 +1,41 @@
-
 export class User {
-    id : string = '';
-    username : string = '';
-    firstname : string = '';
-    lastname : string = '';
-    date_of_birth : string = '';
-    profilePicture : string = '';
-    rating : number = 0;
-    numberOfRatings : number = 0;
-    email : string = '';
-    craft: any;
-    
+  id: number;
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  profilePicture: string;
+  gender: string;
+  role: string;
+
+  constructor(data?: Partial<User>) {
+    this.id = data?.id ?? 0;
+    this.firstname = data?.firstname ?? '';
+    this.lastname = data?.lastname ?? '';
+    this.username = data?.username ?? '';
+    this.email = data?.email ?? '';
+    this.profilePicture = data?.profilePicture ?? '';
+    this.gender = data?.gender ?? '';
+    this.role = data?.role ?? '';
+  }
+}
+
+export class RegularUser extends User {
+  constructor(data?: Partial<RegularUser>) {
+    super(data);
+  }
+}
+
+export class Craftsman extends User {
+  craft: string;
+  rating: number;
+  numberOfRatings: number;
+
+  constructor(data?: Partial<Craftsman>) {
+    super(data);
+
+    this.craft = data?.craft ?? '';
+    this.rating = data?.rating ?? 0;
+    this.numberOfRatings = data?.numberOfRatings ?? 0;
+  }
 }
