ec3be2133180fcab15fa6a28c6735e329f864649
Marko <remaxsrb@protonmail.com>
2026-06-10 10:48:09 +0200

    Rolled back inheritance in user; Added cached user in user service to reduce api calls

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,41 +1,12 @@
 export class User {
-  id: number;
-  firstname: string;
-  lastname: string;
-  username: string;
-  email: string;
-  profilePicture: string;
-  gender: string;
-  role: string;
+    firstname: string = '';
+    lastname: string = '';
+    username: string = '';
+    email: string = '';
+    profilePicture: string = '';
+    gender: string = '';
 
-  constructor(data?: Partial<User>) {
-    this.id = data?.id ?? 0;
-    this.firstname = data?.firstname ?? '';
-    this.lastname = data?.lastname ?? '';
-    this.username = data?.username ?? '';
-    this.email = data?.email ?? '';
-    this.profilePicture = data?.profilePicture ?? '';
-    this.gender = data?.gender ?? '';
-    this.role = data?.role ?? '';
-  }
-}
-
-export class RegularUser extends User {
-  constructor(data?: Partial<RegularUser>) {
-    super(data);
-  }
-}
-
-export class Craftsman extends User {
-  craft: string;
-  rating: number;
-  numberOfRatings: number;
-
-  constructor(data?: Partial<Craftsman>) {
-    super(data);
-
-    this.craft = data?.craft ?? '';
-    this.rating = data?.rating ?? 0;
-    this.numberOfRatings = data?.numberOfRatings ?? 0;
-  }
+    craft?: string;
+    rating?: number;
+    numberOfRatings?: number;
 }
